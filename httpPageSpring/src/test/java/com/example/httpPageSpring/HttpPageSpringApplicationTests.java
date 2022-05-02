7f83a7cc66a7b1d72b15e267e2cd6b824b75a9e6
CristinaUrucu <urucu.cristina24@gmail.com>
2022-05-02 22:29:38 +0300

    Formular first commit

---
+++ httpPageSpring/src/test/java/com/example/httpPageSpring/HttpPageSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpPageSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpPageSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
